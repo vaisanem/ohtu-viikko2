d0c6e582a791252977e0bd96175e56e7b272d334
vaisanem <miikkamv@gmail.com>
2018-11-11 20:01:54 +0200

    master init

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import ohtu.Multiplier;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kuka olet: ");
+        String nimi = scanner.nextLine();
+
+        System.out.println("Hello "+nimi);
+    }
+}
+
