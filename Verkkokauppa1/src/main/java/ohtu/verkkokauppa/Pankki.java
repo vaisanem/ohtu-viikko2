6b8d97105472b504bf2ce4ce3bea15b32cd325d3
vaisanem <miikkamv@gmail.com>
2018-11-13 01:41:43 +0200

    annotations

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -1,9 +1,14 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Component
 public class Pankki implements PankkiInterface {
 
     private KirjanpitoInterface kirjanpito;
 
+    @Autowired
     public Pankki(KirjanpitoInterface kirjanpito) {
         this.kirjanpito = kirjanpito;
     }
